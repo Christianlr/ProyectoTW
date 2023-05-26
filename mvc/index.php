31c7d7eba97dc2a0f6b1202ec2839d251a34c84b
Enrique Araque <minixute@gmail.com>
2023-05-26 14:06:50 +0200

    Prueba para saber si funciona el top de usuarios que mas añaden

--- mvc/index.php
+++ mvc/index.php
@@ -1,9 +1,34 @@
 <?php
 require 'twig/vendor/autoload.php';
-
 require_once "model/UsuarioModel.php";
+require_once "model/IncidenciasModel.php";
+
+
+$loader = new \Twig\Loader\FilesystemLoader('view/html');
+$twig = new \Twig\Environment($loader);
 
 $usuario = new UsuarioModel();
+$incidencia = new IncidenciasModel();
 
-var_dump($tupla);
+$top = $incidencia->getTopUsuarios();
+
+
+$nombre = [];
+$counter = 0;
+foreach ($top as $fila) {
+    $nombre[] = $usuario->getNombreApellidos($fila["id_usuario"]);
+
+    $nombre[$counter] = $nombre[$counter][0]["nombre"] . " " . $nombre[$counter][0]["apellidos"];
+    $counter++;
+}
+
+$total = [];
+foreach ($top as $t)
+    $total[] = $t["count"];
+
+echo $twig->render('default.html', [
+                'nombre' => $nombre,
+                'total' => $total
+                ]);
+
 ?>
