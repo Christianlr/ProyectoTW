c4b7d31d382a4ed9e9c2fd32657c8281eef1c725
Enrique Araque <minixute@gmail.com>
2023-06-27 17:00:11 +0200

    Pagina web v.1.0 terminada

---
+++ Db.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once '../config/config.php';
+
+/* Connect to database */
+class Db {
+    private static $instance;
+    private static $host;
+	private static $db;
+	private static $user;
+	private static $pass;
+    private static $file;
+
+	private function __construct() {}
+
+    private static function connect() {
+        self::$host = constant('DB_HOST');
+		self::$db = constant('DB');
+		self::$user = constant('DB_USER');
+		self::$pass = constant('DB_PASS');
+        self::$file = constant('DB_BACKUPFILE');
+
+		try {
+           $conection = new PDO('mysql:host='.self::$host.'; dbname='.self::$db, self::$user, self::$pass);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit();
+        }
+
+        return $conection;
+    }
+
+    public static function getInstance() {
+        if (!self::$instance)
+            self::$instance = self::connect();
+        
+        return self::$instance;
+    }
+
+    public static function crearCopiaDeSeguridad() {
+        $command = "mysqldump --opt --host=".self::$host. " --user=". self::$user. " --password=" .self::$pass. " " .self::$db. " > " . self::$file;
+        exec($command, $output, $returnVar);
+    
+        if ($returnVar === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function restaurarCopiaDeSeguridad($sql) {
+        try {
+            $pdo = self::$instance;
+    
+            // Iniciar una transacción
+            $pdo->beginTransaction();
+    
+            // Ejecutar las instrucciones SQL
+            $pdo->exec($sql);
+    
+            // Confirmar la transacción
+            $pdo->commit();
+    
+            return true;
+        } catch (PDOException $e) {
+            // Revertir la transacción en caso de error
+            $pdo->rollBack();
+    
+            echo "Error al restaurar la base de datos: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public static function borrarDb() {
+        try {
+            // Establecer la conexión con la base de datos utilizando PDO
+            $pdo = self::$instance;
+    
+            // Obtener el nombre de la base de datos
+            $database = self::$db;
+    
+            // Obtener las tablas de la base de datos
+            $query = "SHOW TABLES";
+            $statement = $pdo->query($query);
+            $tables = $statement->fetchAll(PDO::FETCH_COLUMN);
+    
+            // Desactivar el modo estricto de SQL para evitar errores durante la eliminación de las tablas
+            $pdo->exec("SET sql_mode = ''");
+    
+            // Eliminar cada tabla de la base de datos
+            foreach ($tables as $table) {
+                $pdo->exec("DROP TABLE IF EXISTS $table");
+            }
+            $pdo->exec("DROP TABLE IF EXISTS incidencias");
+            $pdo->exec("DROP TABLE IF EXISTS usuarios");
+
+            return true;
+        } catch (PDOException $e) {
+            echo "Error al borrar las tablas de la base de datos: " . $e->getMessage();
+            return false;
+        }
+    
+        // Cerrar la conexión con la base de datos
+        $pdo = null;
+    }
+}
+
+?>
---
+++ gestionDbController.php
@@ -0,0 +1,57 @@
+<?php
+require '../twig/vendor/autoload.php';
+require_once "../model/Db.php";
+require_once "../model/LogsModel.php";
+
+session_start();
+
+/* Cargamos twig para usar el render */
+$loader = new \Twig\Loader\FilesystemLoader('../view/html');
+$twig = new \Twig\Environment($loader);
+
+//Si no es administrador se redirige al inicio
+if ($_SESSION['datosUsuario']['rol'] != 'administrador') {
+    header('Location: inicioController.php');
+} 
+
+$db = Db::getInstance();
+$log = new LogsModel();
+
+$queryString = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+parse_str($queryString, $params);
+$tipo = '';
+$archivoRestauracion = '';
+if (isset($params['tipo'])) {
+    $tipo = $params['tipo'];
+    if ($tipo == 'restaurar2' && isset($_FILES['archivoSQL']) && $_FILES['archivoSQL']['error'] === UPLOAD_ERR_OK) {
+        $archivoRestauracion = file_get_contents($_FILES['archivoSQL']['tmp_name']);
+    }
+}
+
+
+$archivoRender = 'gestionBBDD.html';
+if ($tipo == 'copia' && Db::crearCopiaDeSeguridad()) {
+    $log->setCopiaSeguridad(date('Y-m-d H:i:s'), $_SESSION['datosUsuario']['email']);
+    $archivoRender = 'confirmacionesBaseDatos.html';
+    $tipo = 'crear';
+} else if ($tipo == 'restaurar1') {
+    $tipo = 'restaurar';
+} else if ($tipo == 'restaurar2' && Db::restaurarCopiaDeSeguridad($archivoRestauracion)) {
+    $log->setRestaurarCopiaSeguridad(date('Y-m-d H:i:s'), $_SESSION['datosUsuario']['email']);
+    header('Location: inicioController.php');
+    $_SESSION = array();
+} else if ($tipo == 'borrar1') {
+    $tipo = 'borrar';
+} else if ($tipo == 'borrar2' && Db::borrarDb()) {
+    $log->setBorrarDb(date('Y-m-d H:i:s'), $_SESSION['datosUsuario']['email']);
+    header('Location: inicioController.php');
+    $_SESSION = array();
+}
+
+
+echo $twig->render($archivoRender, [
+    'ranking' => $_SESSION['ranking'],
+    'datosUsuario' => $_SESSION['datosUsuario'],
+    'tipo' => $tipo
+]);
+?>
